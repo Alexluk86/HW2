eb5cb7b13cd439040b95b76fb3d89727c831d828
Alex <Alex@LAPTOP-TBQG0DAV>
2021-01-31 16:13:04 +0400

    Добавьте файлы проекта.

---
+++ HW2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number1, number2, number3;
+
+            Console.WriteLine("Введите первое число:");
+
+            number1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+
+            number2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите третье число:");
+
+            number3 = double.Parse(Console.ReadLine());
+
+            double sumResult = number1 + number2 + number3;
+
+            double multResult = number1 * number2 * number3;
+
+            Console.WriteLine("Сумма Ваших трех чисел = " + sumResult);
+
+            Console.WriteLine("Произведение Ваших трех чисел = " + multResult);
+
+            Console.ReadLine()
+        }
+    }
+}
